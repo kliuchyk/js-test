34c93a9e11ff764e5b5a4b47cb553167880be847
Evgeny <kliuchyk@gmail.com>
2016-05-31 07:46:50 +0000

    add new file

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-function getAge(age) {
-    var result;
-    if(age<18){
-        result = 'young';
-    } else if (age>60){
-        result='old'
-    } else {
-        result = 'adult';
-    }
-    
-    return result;
+function sqrR(a,b,c) {
+	var D = (b*b) - (4*a*c);
+	if(D>0) {
+		var x1= (-b-Math.sqrt(D))/(2*a);
+		var x2= (-b+Math.sqrt(D))/(2*a);
+	 	alert('x1='+x1);
+		alert('x2= '+x2);
+		return  x1, x2;	
+	} else if (D===0) {
+		var x = -b/(2*a);
+		alert('x= '+x);
+		return x;
+	} else {
+		alert('There is no root');
+	}
 }
 
+var a = Number(prompt("Enter value 'A'"));
+var b = Number(prompt("Enter value 'B'"));
+var c = Number(prompt("Enter value 'C'"));
 
-var age = Number(prompt('how old are you?'));
-var message = 'You are ' + getAge(age);
-alert(message);
-
-
-
-
+sqrR(a,b,c);
